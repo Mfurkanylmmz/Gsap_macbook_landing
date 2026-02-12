74e1ad22b02894ab431dfb47c6d07cfaf2ad53e5
Mustafa Yilmaz <mfurkanylmmz@gmail.com>
2026-02-12 10:45:36 +0300

    ModelSwitcher button control refactor

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useMacbookStore from '../store'
 import { clsx } from 'clsx'
 import { Canvas } from '@react-three/fiber'
@@ -14,6 +14,8 @@ const ProductViewer = () => {
     const { color, scale, setColor, setScale, reset } = useMacbookStore()
 
     const isMobile = useMediaQuery({ query: '(max-width:1024px)' })
+    const [selectedModel, setSelectedModel] = useState('16')
+
 
 
 
@@ -37,13 +39,15 @@ const ProductViewer = () => {
                     </div>
                     <div className='size-control'>
                         <div
-                            onClick={() => setScale(0.06)}
+                            // onClick={() => setScale(0.06)}
+                            onClick={() => setSelectedModel('14')}
                             className={clsx(scale === 0.06 ? "bg-white text-black" : "bg-transparent text-white")}
                         >
                             <p>14"</p>
                         </div>
                         <div
-                            onClick={() => setScale(0.08)}
+                            // onClick={() => setScale(0.08)}
+                            onClick={() => setSelectedModel('16')}
                             className={clsx(scale === 0.08 ? "bg-white text-black" : "bg-transparent text-white")}
                         >
                             <p>16"</p>
@@ -55,7 +59,11 @@ const ProductViewer = () => {
             <Canvas id='canvas' camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
                 <StudioLights />
 
-                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+                {/* <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} /> */}
+                <ModelSwitcher
+                    selectedModel={selectedModel}
+                    isMobile={isMobile}
+                />
             </Canvas>
         </section>
     )
